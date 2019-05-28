b68493e3f4c256e8016675d7626d5bcc75d0c65e
RismaDwiUtami <rismawimi04@gmail.com>
2019-05-28 21:04:41 +0700

    update konfigurasi

--- app/src/main/java/com/example/projectedesa/konfigurasi.java
+++ app/src/main/java/com/example/projectedesa/konfigurasi.java
@@ -6,11 +6,11 @@ public class konfigurasi {
     //Pada tutorial Kali ini, karena kita membuat localhost maka alamatnya tertuju ke IP komputer
     //dimana File PHP tersebut berada
     //PENTING! JANGAN LUPA GANTI IP SESUAI DENGAN IP KOMPUTER DIMANA DATA PHP BERADA
-    public static final String URL_ADD="http://192.168.100.20/Android/pegawai/tambahPgw.php";
-    public static final String URL_GET_ALL = "http://192.168.100.20/Android/pegawai/tampilSemuaPgw.php";
-    public static final String URL_GET_EMP = "http://192.168.100.20/Android/pegawai/tampilPgw.php?id=";
-    public static final String URL_UPDATE_EMP = "http://192.168.100.20/Android/pegawai/updatePgw.php";
-    public static final String URL_DELETE_EMP = "http://192.168.100.20/Android/pegawai/hapusPgw.php?id=";
+    public static final String URL_ADD="http://192.168.1.30/Android/pegawai/tambahPgw.php";
+    public static final String URL_GET_ALL = "http://192.168.1.30/Android/pegawai/tampilSemuaPgw.php";
+    public static final String URL_GET_EMP = "http://192.168.1.30/Android/pegawai/tampilPgw.php?id=";
+    public static final String URL_UPDATE_EMP = "http://192.168.1.30/Android/pegawai/updatePgw.php";
+    public static final String URL_DELETE_EMP = "http://192.168.1.30/Android/pegawai/hapusPgw.php?id=";
 
     //Dibawah ini merupakan Kunci yang akan digunakan untuk mengirim permintaan ke Skrip PHP
     public static final String KEY_EMP_ID = "id";
@@ -30,8 +30,8 @@ public class konfigurasi {
     public static final String EMP_ID = "emp_id";
 
     // URL DATABASE R DESA
-    public static final String URL_PENGUMUMAN_ALL = "http://192.168.100.20/GitHub/ProjectWebService/api/C_pengumuman";
-    public static final String URL_PERMOHONAN_ONE = "http://192.168.100.20/GitHub/ProjectWebService/api/c_permohonan";
+    public static final String URL_PENGUMUMAN_ALL = "http://192.168.1.30/GitHub/ProjectWebService/api/C_pengumuman";
+    public static final String URL_PERMOHONAN_ONE = "http://192.168.1.30/GitHub/ProjectWebService/api/c_permohonan";
 
     // JSON TAGS PENGUMUMAN
     public static final String TAG_ID_PENGUMUMAN = "id_pengumuman";
