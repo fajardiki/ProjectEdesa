c79f655094a62bf87667760c95bfec8483e32194
RismaDwiUtami <rismawimi04@gmail.com>
2019-06-15 10:49:26 +0700

    ipconfig

--- app/src/main/java/com/example/projectedesa/LoginActivity.java
+++ app/src/main/java/com/example/projectedesa/LoginActivity.java
@@ -30,7 +30,7 @@ public class LoginActivity extends AppCompatActivity {
     private Button btn_login;
     private ProgressBar loading;
 //    192.168.100.14
-    private static String URL_LOGIN = "http://192.168.100.14/GitHub/ProjectWebService/api/c_login";
+    private static String URL_LOGIN = "http://192.168.1.44/GitHub/ProjectWebService/api/c_login";
     SessionManager sessionManager;
 
     @Override
